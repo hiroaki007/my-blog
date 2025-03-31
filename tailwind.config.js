f10ec1badd86dd309aa562149ee396e8b494a53c
hiroaki <blackcoff0722@gmail.com>
2025-03-31 21:44:21 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",   // App Router 用
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
